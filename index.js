51f329e68902be861d62de3d145cc9e824e6d007
Bharat Mali <bharatmali1704@gmail.com>
2025-06-11 10:11:26 +0530

    updated code

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 25;
+
+if(a<15){
+    console.log("a is less than 15")
+}
+
+else if(a>15){
+    console.log("a is greater than 15")
+}
+
+else if(a==15){
+    console.log("a equals to 15")
+}
